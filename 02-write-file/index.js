f844969d162890d07a2aabe16f950260805422dd
Webs95 <alexandersemashko1995@yandex.ru>
2022-11-05 20:11:55 +0300

    feat: add solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+const { stdin, stdout, exit } = require('process');
+const resultFile = fs.createWriteStream(filePath);
+const readLine = require('readline').createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+function closeScript() {
+  readLine.write(`Bye-bye! :)`);
+  readLine.close();
+  resultFile.end();
+  exit(0);
+}
+
+readLine.write(`Please, type text for new file. \n`);
+readLine.on('line', (value) =>
+  value.trim().toLocaleLowerCase() === 'exit'
+    ? closeScript()
+    : resultFile.write(`${value} \n`)
+);
