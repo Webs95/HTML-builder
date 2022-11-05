6cdc162f8add8f962ca26a36ea7d9fcd6a05c88b
Webs95 <alexandersemashko1995@yandex.ru>
2022-11-05 14:44:09 +0300

    refactor: prettier

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
 const path = require('path');
+const { log } = console;
 const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
-readStream.on('data', (dataPart) => console.log(dataPart.toString()));
+readStream.on('data', (dataPart) => log(dataPart.toString()));
